11b64a5eeb3f9f6efcdf598ad27cc0ddb77c16dd
Jaroslav <jaroslav1h3best@gmail.com>
2018-03-05 21:04:03 +0200

    add friendRequest, confirm or delete reauest, improvement friends view

---
+++ src/main/java/com/alwaysnearyou/entity/Room.java
@@ -0,0 +1,33 @@
+package com.alwaysnearyou.entity;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private Date rimeCreated;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "room_user",
+            joinColumns = @JoinColumn(name = "room_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"))
+    private List<User> usersInRoom;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
+    private List <Message> message;
+
+}
