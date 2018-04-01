23b29fe36d2e7993bc63e925be4335f63217d46d
yarik <jaroslav1h3best@gmail.com>
2018-04-01 20:51:11 +0300

    Split controllers

---
+++ src/main/java/com/alwaysnearyou/controller/LoginController.java
@@ -0,0 +1,113 @@
+package com.alwaysnearyou.controller;
+
+import com.alwaysnearyou.dao.UserDAO;
+import com.alwaysnearyou.entity.User;
+import com.alwaysnearyou.service.MessageService;
+import com.alwaysnearyou.service.UserService;
+import com.alwaysnearyou.service.impl.MailServiceImpl;
+import com.alwaysnearyou.service.impl.RoomServiceImpl;
+import com.alwaysnearyou.utils.RandomCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private RoomServiceImpl roomService;
+
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private MailServiceImpl mailService;
+
+    @RequestMapping(value= "/new", method = RequestMethod.GET)
+    public String getCreateAccountPage() {
+        return "createNewAccount";
+    }
+
+    @RequestMapping(value= "/save", method = RequestMethod.POST) // TODO: use /login/save instead
+    public String save(@RequestParam String name, @RequestParam String surname,
+                       @RequestParam String birthday, @RequestParam String password,
+                       @RequestParam String gender, @RequestParam String country,
+                       @RequestParam String address, @RequestParam String email,
+                       @RequestParam int phone, @RequestParam MultipartFile avatar, HttpSession session,ModelMap model){
+
+        String path = System.getProperty("user.home") + File.separator + "Avatars\\";
+        try {
+            avatar.transferTo(new File(path + avatar.getOriginalFilename()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        RandomCode randomCode = new RandomCode();
+        int code = randomCode.randomNumber();
+        User user = new User();
+        user.setName(name);
+        user.setSurname(surname);
+        user.setBirthday(birthday);
+        user.setPassword(password);
+        user.setGender(gender);
+        user.setCountry(country);
+        user.setAddress(address);
+        user.setEmail(email);
+        user.setPhone(phone);
+        user.setAvatar("\\userAvatar\\" + avatar.getOriginalFilename());
+        user.setCode(code);
+        System.out.println(user);
+
+        userService.save(user);
+        mailService.send(email, code);
+        session.setAttribute("user",user.getId());
+        return "confirmPage";
+    }
+
+    @RequestMapping(value= "/confirm", method = RequestMethod.POST)
+    public String confirm(@RequestParam int confirm, HttpSession session,ModelMap model){
+        Integer id = (Integer) session.getAttribute("user");
+        User user = userDAO.findUserById(id);
+        int userCode = user.getCode();
+        if (confirm == userCode){
+            user.setActive(true);
+            userService.save(user);
+            session.setAttribute("user",user.getId());
+            model.addAttribute("user", user);
+            return "redirect:/mainPage";
+        } else {
+            System.out.println("ERRRRROOOOOORR");
+            return null;
+        }
+    }
+
+    @RequestMapping(value= "/logIn", method = RequestMethod.POST)
+    public String logIn( @RequestParam("signinEmail") String email,
+                         @RequestParam("signinPassword") String password, HttpSession session,ModelMap model){
+        User user = userService.findUserByEmailAndPassword(email, password);
+        if(user != null) {
+            session.setAttribute("user", user.getId());
+            return "redirect:/mainPage";
+        }else{
+            return "errorSingnIn";
+        }
+    }
+
+    @RequestMapping(value= "/logIn", method = RequestMethod.GET)
+    public String logIn(){
+        return "login";
+    }
+}
---
+++ src/main/java/com/alwaysnearyou/controller/filters/UserFilter.java
@@ -0,0 +1,40 @@
+package com.alwaysnearyou.controller.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class UserFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession(false);
+        String loginURI = request.getContextPath() + "/logIn";
+
+        boolean loggedIn = session != null && session.getAttribute("user") != null;
+        boolean loginRequest = request.getRequestURI().equals(loginURI);
+
+        if (loggedIn || loginRequest) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.sendRedirect(loginURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        //TODO: nothing here
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        //TODO: nothing here
+    }
+
+}
